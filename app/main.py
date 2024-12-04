b604b41477992b0f599f67742e4266470509cac3
KobbyKobby1 <99130349+KobbyKobby1@users.noreply.github.com>
2024-12-04 18:20:52 +0000

    Updates to main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 import sys
 
-
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-
-    # Wait for user input
-    input()
-    command = input()
-    print(f"{command}: command not found")
-
+    while True:  # Allows the shell to repeatedly take commands
+        sys.stdout.write("$ ")  # Display the shell prompt
+        sys.stdout.flush()
+        
+        command = input()  # Get user input
+        
+        if command.strip():  # Check if input is not empty
+            print(f"{command}: command not found")
+        else:
+            break  # Exit the loop if no command is entered
 
 if __name__ == "__main__":
     main()
